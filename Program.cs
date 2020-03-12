ac81fa663a514e9ea16b7725f9db4dc98cbb03b0
florentina-ban <52647090+florentina-ban@users.noreply.github.com>
2020-03-12 06:50:32 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ContestAppC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
